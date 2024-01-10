dc6cba488cda8aeda62e6d0cd288297a541f9716
Ambady Ajay <ambady.ajay@altd.in>
2024-01-10 10:39:48 +0530

    Added my project

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,20 @@
+import Button from "../components/Button"
+
+const Hero = () => {
+  return (
+   <section id="home" className=" w-full  flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
+<div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-lg:paddding-x pt-28">
+<p className="text-xl font-montserrat text-coral-red">   Our Summer collections</p>
+<h1 className=" mt-10 font-palanquin text-8xl"><span> The New Arrival</span>
+<br />
+<span>Nike</span> shoes
+</h1>
+<p> Discover stylish Nike arrivals, quality comfort, and innovation for
+          your active life.</p>
+          <Button/>
+</div>
+   </section>
+  )
+}
+
+export default Hero
